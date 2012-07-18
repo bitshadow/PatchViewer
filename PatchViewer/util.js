7c796de32235ea25b0aef4bfa4f7090af97f451f
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2012-07-18 13:27:56 +0530

    Added license

--- PatchViewer/util.js
+++ PatchViewer/util.js
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2011 Jignesh Kakadiya <jigneshhk1992@gmail.com>
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+*/
+
 /**
 
 String.prototype.endsWith = function(suffix) {
@@ -17,6 +36,10 @@ String.prototype.trim = function() {
 String.prototype.startsWith = function(str) {
   return (this.match("^"+str)==str)
 }
+
+/*String.prototype.startsWith = function (str) {
+  return !this.indexOf(str);
+}*/
 
 String.prototype.endsWith = function(str) {
   return (this.match(str+"$")==str)
